01744c8fe0a68acfc420fa1226e490a0854ddcb8
qoxogus <a01034367055@gmail.com>
2021-06-07 20:36:52 +0900

    [ADD] JPA, DB 설정

--- src/test/java/studty/querydsl/QuerydslApplicationTests.java
+++ src/test/java/studty/querydsl/QuerydslApplicationTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.jpa.repository.query.JpaQueryCreator;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import studty.querydsl.entity.Hello;
 import studty.querydsl.entity.QHello;
@@ -16,6 +17,7 @@ import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
+@Commit
 class QuerydslApplicationTests {
 
 	@Autowired
