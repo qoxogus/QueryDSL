5b0e0fd8d96b97cb2f936294115c147c85fa5807
qoxogus <a01034367055@gmail.com>
2021-06-08 23:09:34 +0900

    [ADD] 검색조건 쿼리

--- src/test/java/studty/querydsl/QuerydslBasicTest.java
+++ src/test/java/studty/querydsl/QuerydslBasicTest.java
@@ -77,4 +77,47 @@ public class QuerydslBasicTest {
         assertThat(findMember.getUsername()).isEqualTo("member1");
 
     }
+
+    @Test
+    public void search() {
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1")
+                        .and(member.age.eq(10)))
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+
+        //member.username.eq  / username = 'member1'
+        //member.username.ne  / username != 'member1'
+        //member.username.eq("member1").not()  / username != 'member1'
+
+        //member.username.isNotNull()   // 이름이 isNotNull
+
+        //member.age.in(10, 20)   /  age in (10, 20)
+        //member.age.notIn(10, 20) / age not in (10, 20)
+        //member.age.between(10, 30) / between 10, 30
+
+        //member.age.goe(30)  /  age >= 30
+        //member.age.gt(30)  /  age > 30
+        //member.age.loe(30)  /  age <= 30
+        //member.age.lt(30)  /  age < 30
+
+        //member.username.like("member%")  /  like 검색
+        //member.username.contains("member")  /  like '%member%' 검색
+        //member.username.startsWith("member")  /  like 'member%' 검색
+    }
+
+    @Test
+    public void searchAndParam() {
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(
+                        member.username.eq("member1"), //and로 연결한 것과 같다 (중간에 null이 들어가면 null을 무시한다 {동적쿼리 만들 때 이를 활용하여 기가막히게 코드를 깔끔하게 짤 수 있는 방법이 있다 (이 방법을 선호한다)}
+                        member.age.eq(10)
+                )
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
 }
