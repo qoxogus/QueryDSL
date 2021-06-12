7045b012f7d54dfd493a57e5a9388122c72244df
qoxogus <a01034367055@gmail.com>
2021-06-13 00:34:17 +0900

    [ADD] 프로젝션과 결과 반환 - 기본

--- src/test/java/studty/querydsl/QuerydslBasicTest.java
+++ src/test/java/studty/querydsl/QuerydslBasicTest.java
@@ -510,4 +510,31 @@ public class QuerydslBasicTest {
             System.out.println("s = " + s); //member1_10
         }
     }
+
+    @Test
+    public void simpleProjection() {
+        List<String> result = queryFactory
+                .select(member.username)
+                .from(member)
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    @Test
+    public void tupleProjection() {
+        List<Tuple> result = queryFactory
+                .select(member.username, member.age)
+                .from(member)
+                .fetch();
+
+        for (Tuple tuple : result) {  //Tuple은 DTO로 바꿔서 반환해주자
+            String username = tuple.get(member.username);
+            Integer age = tuple.get(member.age);
+            System.out.println("username = " + username);
+            System.out.println("age = " + age);
+        }
+    }
 }
