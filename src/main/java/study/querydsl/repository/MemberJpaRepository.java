e74d4631f0da9b3cde8170cd422355d5bf68780c
qoxogus <a01034367055@gmail.com>
2021-07-03 22:38:21 +0900

    [ADD] 순수JPA 리포지토리, Querydsl

---
+++ src/main/java/study/querydsl/repository/MemberJpaRepository.java
@@ -0,0 +1,60 @@
+package study.querydsl.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+import static study.querydsl.entity.QMember.*;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberJpaRepository {
+
+    private final EntityManager em;
+    private final JPAQueryFactory queryFactory;
+
+//    public MemberJpaRepository(EntityManager em, JPAQueryFactory queryFactory) {
+//        this.em = em;
+////        this.queryFactory = new JPAQueryFactory(); //이 방법은 @RequiredArgsConstructor 어노테이션을 쓰지 못함
+//        this.queryFactory = queryFactory; //이 방법으로 하려면 application에 Bean추가 해야함
+//    }
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Optional<Member> findById(Long id) {
+        Member findMember = em.find(Member.class, id);
+        return Optional.ofNullable(findMember);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findAll_Querydsl() {
+        return queryFactory
+                .selectFrom(member)
+                .fetch();
+    }
+
+    public List<Member> findByUsername(String username) {
+        return em.createQuery("select m from Member m where m.username = :username", Member.class)
+                .setParameter("username", username)
+                .getResultList();
+    }
+
+    public List<Member> findByUsername_Querydsl(String username) {
+        return queryFactory
+                .selectFrom(member)
+                .where(member.username.eq(username))
+                .fetch();
+    }
+}
